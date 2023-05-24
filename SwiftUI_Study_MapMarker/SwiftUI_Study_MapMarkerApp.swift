3337d4f01e97acbbb93267def69478fd3c3f7c41
JeongAYoo <jeonga038@naver.com>
2023-05-24 14:57:53 +0900

    Initial Commit

---
+++ SwiftUI_Study_MapMarker/SwiftUI_Study_MapMarkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Study_MapMarkerApp.swift
+//  SwiftUI_Study_MapMarker
+//
+//  Created by Jade Yoo on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Study_MapMarkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
